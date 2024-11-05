0bbd0a5419d79e7e8e8097e7ef0034ab671eb51b
wipodev <ajwipo@gmail.com>
2024-11-05 15:46:03 -0500

    created a startup script to create new projects

---
+++ scripts/init.js
@@ -0,0 +1,23 @@
+import fs from "fs";
+import path from "path";
+import { copy } from "../lib/utils.js";
+
+const packageJsonPath = path.resolve(process.cwd(), "package.json");
+
+export function init() {
+  console.log("Initializing the project...");
+  copy("../template", ".");
+
+  try {
+    const pkg = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
+    pkg.scripts = {
+      ...pkg.scripts,
+      dev: "npx wiview dev",
+      build: "npx wiview build",
+    };
+    fs.writeFileSync(packageJsonPath, JSON.stringify(pkg, null, 2), "utf8");
+    console.log("Project initialized.");
+  } catch (err) {
+    console.error("Error initializing the project:", err);
+  }
+}
