e214640aca1466702b665fd7b4ed6368e528a3ef
wipodev <ajwipo@gmail.com>
2024-11-06 19:32:49 -0500

    Customizing routes based on function-level parameters

--- scripts/dev.js
+++ scripts/dev.js
@@ -3,10 +3,10 @@ import { exec } from "child_process";
 import { join } from "path";
 import { copy } from "../lib/utils.js";
 
-export function modeDev() {
-  copy("../src/core.js", "scripts/wiview.js");
+export function modeDev(basePath, baseModulePath) {
+  copy(`${baseModulePath}/core.js`, `${basePath}/scripts/wiview.js`);
 
-  const servorProcess = exec("servor --reload");
+  const servorProcess = exec(`servor ${basePath} index.html --reload`);
 
   servorProcess.stdout.on("data", (data) => {
     console.log(data.toString());
@@ -20,7 +20,7 @@ export function modeDev() {
     console.log("\nClosing the server...");
     servorProcess.kill("SIGINT");
     try {
-      rmSync(join(process.cwd(), "/scripts/wiview.js"));
+      rmSync(join(process.cwd(), `${basePath}/scripts/wiview.js`));
     } catch (err) {
       console.error("Error deleting file:", err);
     }
