0d5fb886e320ad29eb6e3697d41c61b3792d08ec
wipodev <ajwipo@gmail.com>
2024-11-13 22:44:15 -0500

    Configuration change and addition of Vite as a tool for compilation and development

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: "./lib/core.js",
+      name: "wiview",
+      fileName: (format) => `wiview.${format}.js`,
+      formats: ["es", "cjs"],
+    },
+    rollupOptions: {
+      external: ["commander", "vite", "gh-pages"],
+      output: {
+        globals: {
+          commander: "commander",
+          ghPages: "gh-pages",
+          vite: "vite",
+        },
+      },
+    },
+  },
+});
