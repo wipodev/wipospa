1e1169831cc6de7eea887498e6efbcff957eb630
wipodev <ajwipo@gmail.com>
2024-11-19 16:30:22 -0500

    change of route in imports

--- template/src/app.js
+++ template/src/app.js
@@ -1,6 +1,6 @@
 import { registerComponent, Router } from "wiview";
-import { components } from "./defineComponents.js";
-import { routes } from "./routes/defineRoutes.js";
+import { components } from "./config/defineComponents.js";
+import { routes } from "./config/defineRoutes.js";
 
 registerComponent(components);
 const router = Router(routes);
