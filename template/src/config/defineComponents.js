06ddad3b4334cd1690a4a14e898e6cc8b437f4cf
wipodev <ajwipo@gmail.com>
2024-11-19 19:38:44 -0500

    corrected import routes

--- template/src/config/defineComponents.js
+++ template/src/config/defineComponents.js
@@ -1,10 +1,10 @@
-import TitleLib from "./Title_Lib.html";
-import DarkToggle from "./Dark_Toggle.html";
-import HeaderPage from "./Header_Page.html";
-import FooterPage from "./Footer_Page.html";
-import Layout from "../Layout.html";
-import Home from "./home.html";
-import About from "./about.html";
+import TitleLib from "../app/components/Title_Lib.html";
+import DarkToggle from "../app/components/Dark_Toggle.html";
+import HeaderPage from "../app/components/Header_Page.html";
+import FooterPage from "../app/components/Footer_Page.html";
+import Layout from "../app/Layout.html";
+import Home from "../app/views/home.html";
+import About from "../app/views/about.html";
 
 export const components = {
   TitleLib: { template: TitleLib },
