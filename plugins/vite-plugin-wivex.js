e79a42593a12f6da2017ce45be7308b32c39dff6
wipodev <ajwipo@gmail.com>
2024-11-28 22:23:28 -0500

    Reordering plugin to work with new compiler

--- plugins/vite-plugin-wivex.js
+++ plugins/vite-plugin-wivex.js
@@ -1,27 +1,16 @@
-import fs from "fs";
-import compileComponent from "../lib/compileComponents.js";
+import compileComponent from "../lib2/componentCompiler.js";
 
 export function WivexCompiler() {
-  const root = path.posix.join(process.cwd().replace(/\\/g, "/"), "node_modules/.vite/deps/wivex.js");
   return {
     name: "wivex-compiler",
     apply: "serve",
     transform(code, id) {
       if (id.endsWith(".html")) {
-        const compiledCode = compileComponent(code);
-
-        const map = {
-          version: 3,
-          file: id,
-          sources: [id],
-          names: ["getState"],
-          mappings:
-            "AAAA,SAASA,OAAO,CAAC,GAAR,CAAY,SAAZ,CAAA,CAAA;AAGzB,OAAO,CAAC,GAAR,CAAY,SAAZ,CAAA,CAAA,OAAO,CAAC,GAAR,CAAY,SAAR,CAAA,CAAA,CAAA,OAAO,OAAO,OAAO,OAAO,OAAO,CAAA,OAAO,OAAO,OAAO,OAAO,CAAA,OAAO,OAAO,CAAA,OAAO,OAAO,OAAO,OAAO,OAAO;AAAA",
-        };
+        const compiledCode = compileComponent(code, id);
 
         return {
           code: compiledCode,
-          map: map,
+          map: null,
         };
       }
       return null;
