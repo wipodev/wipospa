bf82f57d1f11a6446838a17fa7c9d085c70e9549
wipodev <ajwipo@gmail.com>
2024-12-05 08:17:27 -0500

    Correction of TextContent processes which replace the content of the tags with the text

--- plugins/wivex-compiler/process/elementCreator.js
+++ plugins/wivex-compiler/process/elementCreator.js
@@ -29,37 +29,47 @@ function processInnerHTML(tagName, index, innerHTML, resolveReactiveKey, getName
 
     children.each((i, el) => {
       if (el.type === "text") {
-        const textContent = $(el).text().trim();
+        const textContent = $(el).text();
         if (textContent) {
-          innerHTMLCode += processTextContent(tagName, index, textContent, resolveReactiveKey);
+          innerHTMLCode += processTextContent(tagName, index, textContent, resolveReactiveKey, i);
         }
       } else if (el.type === "tag") {
         innerHTMLCode += processElement($, el, `${i}${index}`, `${tagName}${index}`, resolveReactiveKey, getName);
       }
     });
   } else {
-    innerHTMLCode += processTextContent(tagName, index, innerHTML, resolveReactiveKey);
+    innerHTMLCode += processTextContent(tagName, index, innerHTML, resolveReactiveKey, index);
   }
 
   return innerHTMLCode;
 }
 
-function processTextContent(tagName, index, innerHTML, resolveReactiveKey) {
+function processTextContent(tagName, index, innerHTML, resolveReactiveKey, subIndex) {
   if (!innerHTML) return "";
+
+  const textNodeName = `${tagName}${index}TextNode${subIndex}`;
   if (innerHTML.includes("{")) {
     const reactiveContent = innerHTML.replace(/{(.*?)}/g, (_, content) =>
       resolvedAnidedKey(content, resolveReactiveKey)
     );
     if (/^\s*{.*}\s*$/.test(innerHTML)) {
-      return `${tagName}${index}.textContent = ${reactiveContent};\n`;
+      return (
+        `const ${textNodeName} = document.createTextNode(${reactiveContent});\n` +
+        `${tagName}${index}.appendChild(${textNodeName});\n`
+      );
     } else {
-      return `${tagName}${index}.textContent = \`${innerHTML.replace(
-        /{(.*?)}/g,
-        (_, content) => `\${${resolvedAnidedKey(content, resolveReactiveKey)}}`
-      )}\`;\n`;
+      return (
+        `const ${textNodeName} = document.createTextNode(\`${innerHTML.replace(
+          /{(.*?)}/g,
+          (_, content) => `\${${resolvedAnidedKey(content, resolveReactiveKey)}}`
+        )}\`);\n` + `${tagName}${index}.appendChild(${textNodeName});\n`
+      );
     }
   } else {
-    return `${tagName}${index}.textContent = \`${innerHTML}\`;\n`;
+    return (
+      `const ${textNodeName} = document.createTextNode(\`${innerHTML}\`);\n` +
+      `${tagName}${index}.appendChild(${textNodeName});\n`
+    );
   }
 }
 
