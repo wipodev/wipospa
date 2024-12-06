1d220d8dce636de171feb32e0c6cb5afa695ec5b
wipodev <ajwipo@gmail.com>
2024-12-05 20:00:20 -0500

    Refactored and bug fixed in classTemplate

--- plugins/wivex-compiler/template/classTemplate.js
+++ plugins/wivex-compiler/template/classTemplate.js
@@ -10,21 +10,8 @@ export function generateClassTemplate({
   templateContent,
   container,
 }) {
-  return `${imports}
-
-class ${componentName} {
-  constructor(props = {}) {
-    this.id = Math.random().toString(36).substring(2, 9);
-    ${props}
-    this.subscriptions = {};
-    this.state = {};
-    ${state}
-    ${subscriptions}
-    ${bindMethods}
-    ${styleContent ? "this.ensureStyles();" : ""}
-  }
-
-  defineReactiveProperty(obj, key, initialValue) {
+  const defineReactiveProperty = state
+    ? `defineReactiveProperty(obj, key, initialValue) {
     let value = initialValue;
     this.subscriptions[key] = [];
     Object.defineProperty(obj, key, {
@@ -32,60 +19,82 @@ class ${componentName} {
       set: (newValue) => {
         value = newValue;
         this.subscriptions[key].forEach((callback) => callback());
-      },
+      }
     });
+  }`
+    : "";
+
+  const updateComponent = state
+    ? `updateComponent(currentNode, newNode) {
+    if (!currentNode || !newNode) return;
+
+    if (currentNode.nodeType === Node.TEXT_NODE && currentNode.textContent !== newNode.textContent) {
+      currentNode.textContent = newNode.textContent;
+    } else if (currentNode.nodeType === Node.ELEMENT_NODE && currentNode.tagName !== newNode.tagName) {
+      currentNode.replaceWith(newNode.cloneNode(true));
+    } else if (currentNode.nodeType === Node.ELEMENT_NODE) {
+      Array.from(newNode.attributes).forEach((attr) => {
+        if (currentNode.getAttribute(attr.name) !== attr.value) {
+          currentNode.setAttribute(attr.name, attr.value);
+        }
+      });
+
+      const currentChildren = Array.from(currentNode.childNodes);
+      const newChildren = Array.from(newNode.childNodes);
+
+      const maxLen = Math.max(currentChildren.length, newChildren.length);
+      for (let i = 0; i < maxLen; i++) {
+        if (newChildren[i] && !currentChildren[i]) {
+          currentNode.appendChild(newChildren[i].cloneNode(true));
+        } else if (currentChildren[i] && !newChildren[i]) {
+          currentNode.removeChild(currentChildren[i]);
+        } else {
+          this.updateComponent(currentChildren[i], newChildren[i]);
+        }
+      }
+    }
+  }`
+    : "";
+
+  const reRender = state
+    ? `else {
+      const el = document.querySelector(\`[data-component-id="${componentName}-\${this.id}"]\`);
+      this.updateComponent(el, ${container});
+    }`
+    : "";
+
+  return `${imports}
+
+class ${componentName} {
+  constructor(props = {}) {
+    this.id = Math.random().toString(36).substring(2, 9);
+    ${props}${subscriptions ? "\nthis.subscriptions = {};" : ""}${
+    state ? `\nthis.state = {};\n${state}` : ""
+  }${subscriptions}${bindMethods}${styleContent ? "\nthis.ensureStyles();" : ""}
   }
+
+  ${defineReactiveProperty}
 
   ${styleContent}
 
   ${methods}
 
-  updateComponent(currentRender, newRender) {
-    if (!currentRender || !newRender) return;
-
-    const currentChildren = Array.from(currentRender.childNodes);
-    const newChildren = Array.from(newRender.childNodes);
-
-    newChildren.forEach((newChild, index) => {
-      if (!newChild.hasAttribute("data-component-id")) {
-        const currentChild = currentChildren[index];
-
-        if (currentChild && currentChild.isEqualNode(newChild)) {
-          return;
-        }
-
-        const existingNode = currentChildren.find((child) => child.isEqualNode(newChild));
-        if (existingNode) {
-          currentRender.insertBefore(existingNode, currentChild || null);
-        } else {
-          currentRender.insertBefore(newChild.cloneNode(true), currentChild || null);
-        }
-      }
-    });
-
-    currentChildren.forEach((currentChild) => {
-      if (!currentChild.hasAttribute("data-component-id")) {
-        if (!newChildren.some((newChild) => newChild.isEqualNode(currentChild))) {
-          currentRender.removeChild(currentChild);
-        }
-      }
-    });
-  }
+  ${updateComponent}
 
   render(container = null) {
     ${templateContent}
 
     if (container) {
       container.appendChild(${container});
-    } else {
-      const el = document.querySelector(\`[data-component-id="${componentName}-\${this.id}"]\`);
-      this.updateComponent(el, ${container});
-    }
+    } ${reRender}
 
     return ${container};
   }
 
-  mount(container) {
+  mount(container, replace = false) {
+    if (replace && container) {
+        container.innerHTML = "";
+    }
     this.render(container);
   }
 }
